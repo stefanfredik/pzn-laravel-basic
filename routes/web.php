dc26e03c95d35397cdd61953972964b108f50817
stefanfredik <stefanfredik@gmail.com>
2023-08-05 21:51:17 +0800

    reuest input filter

--- routes/web.php
+++ routes/web.php
@@ -21,63 +21,63 @@ Route::get('/', function () {
 
 
 // get
-Route::get('/kflores',function(){
+Route::get('/kflores', function () {
     return "Hello Kopi FLores";
 });
 
 // redirect
-Route::redirect('/youtube','/kflores');
+Route::redirect('/youtube', '/kflores');
 
 // fallback
-Route::fallback(function(){
+Route::fallback(function () {
     return "404";
 });
 
-Route::view('/hello','hello.world',['name'=> 'Fredik Stefan']);
+Route::view('/hello', 'hello.world', ['name' => 'Fredik Stefan']);
 
-Route::get('/hello-again',function(){
-    return view('hello',[ 'name'=>'Fredik Stefan']);
+Route::get('/hello-again', function () {
+    return view('hello', ['name' => 'Fredik Stefan']);
 });
 
 // Routing id
-Route::get("/products/{id}",function($productId){
+Route::get("/products/{id}", function ($productId) {
     return "Product $productId";
 })->name('product.detail');
 
 
 // routing with double params
-Route::get("/products/{id}/items/{itemId}",function($productId,$itemId){
+Route::get("/products/{id}/items/{itemId}", function ($productId, $itemId) {
     return "Product $productId, Item $itemId";
 })->name('product.item.detail');
 
 
 // routing with regex
-Route::get('/categories/{id}',function($categoryId){
+Route::get('/categories/{id}', function ($categoryId) {
     return "Category $categoryId";
-})->where('id','[0-9]+');
+})->where('id', '[0-9]+');
 
-Route::get("/users/{id?}",function($userId = "404"){
+Route::get("/users/{id?}", function ($userId = "404") {
     return "User $userId";
 })->name('user.detail');
 
 // route conflict
-Route::get("/conflict/fredik",function(){
+Route::get("/conflict/fredik", function () {
     return "Conflict Fredik Stefan";
 });
 
-Route::get('/conflict/{name}',function($name){
+Route::get('/conflict/{name}', function ($name) {
     return "Conflict $name";
 });
 
 // named router
 
-Route::get("/produk/{id}",function($id){
-    $link = route('product.detail',['id' => $id]);
+Route::get("/produk/{id}", function ($id) {
+    $link = route('product.detail', ['id' => $id]);
     return "Link $link";
 });
 
-Route::get("/produk-redirect/{id}",function($id){
-    return redirect()->route('product.detail',['id'=> $id]);
+Route::get("/produk-redirect/{id}", function ($id) {
+    return redirect()->route('product.detail', ['id' => $id]);
 });
 
 // COntroller Route
@@ -85,12 +85,14 @@ Route::get('/controller/hello/request', [HelloController::class, 'request']);
 Route::get('/controller/hello/{name}', [HelloController::class, 'hello']);
 
 // Input Request Route
-Route::get('/input/hello',[InputController::class,'hello']);
-Route::post('/input/hello',[InputController::class,'hello']);
-Route::post('/input/hello/first',[InputController::class,'helloFirstName']);
-Route::post('/input/hello/input',[InputController::class,'helloInput']);
-Route::post('/input/hello/input',[InputController::class,'helloInput']);
-Route::post('/input/hello/array',[InputController::class,'helloArray']);
-Route::post('/input/type',[InputController::class,'inputType']);
+Route::get('/input/hello', [InputController::class, 'hello']);
+Route::post('/input/hello', [InputController::class, 'hello']);
+Route::post('/input/hello/first', [InputController::class, 'helloFirstName']);
+Route::post('/input/hello/input', [InputController::class, 'helloInput']);
+Route::post('/input/hello/input', [InputController::class, 'helloInput']);
+Route::post('/input/hello/array', [InputController::class, 'helloArray']);
+Route::post('/input/type', [InputController::class, 'inputType']);
 
-
+Route::post('/input/filter/only', [InputController::class, 'filterOnly']);
+Route::post('/input/filter/except', [InputController::class, 'filterExcept']);
+Route::post('/input/filter/merge', [InputController::class, 'filterMerge']);
--- tests/Feature/InputControllerTest.php
+++ tests/Feature/InputControllerTest.php
@@ -6,37 +6,36 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class InputControllerTest extends TestCase
-{
-    function testInput(){
+class InputControllerTest extends TestCase {
+    function testInput() {
         $this->get("/input/hello?name=fredik")->assertSeeText("Hello fredik");
-        $this->post("/input/hello",[
+        $this->post("/input/hello", [
             'name' => 'Fredik'
         ])->assertSeeText('Hello Fredik');
     }
 
 
-    public function testInputNested(){
-        $this->post('/input/hello/first',[
+    public function testInputNested() {
+        $this->post('/input/hello/first', [
             'name' => [
                 'first' => 'Fredik',
                 'last' => 'Stefan'
             ]
-            ])->assertSeeText('Hello Fredik');
+        ])->assertSeeText('Hello Fredik');
     }
 
-    public function testInputAll(){
-        $this->post('/input/hello/input',[
+    public function testInputAll() {
+        $this->post('/input/hello/input', [
             'name' => [
                 'first' => 'Fredik',
                 'last' => 'Stefan'
             ]
-            ])->assertSeeText('Fredik')->assertSeeText("Stefan");
+        ])->assertSeeText('Fredik')->assertSeeText("Stefan");
     }
 
-    public function testInputArray(){
-        $this->post('/input/hello/products',[
-            'products'=>[
+    public function testInputArray() {
+        $this->post('/input/hello/products', [
+            'products' => [
                 [
                     'name' => "Apple Mac Book Pro",
                     "price" => 30000000
@@ -46,11 +45,11 @@ class InputControllerTest extends TestCase
                     'price' => 10000000
                 ]
             ]
-            ])->assertSeeText('Apple')->assertSeeText("Samsung");
+        ])->assertSeeText('Apple')->assertSeeText("Samsung");
     }
 
 
-    public function testInputType(){
+    public function testInputType() {
         $this->post(
             '/input/type',
             [
@@ -59,5 +58,39 @@ class InputControllerTest extends TestCase
                 'birth_date' => '1990-10-10'
             ]
         )->assertSeeText('Budi')->assertSeeText('true')->assertSeeText('1990-10-10');
+    }
+
+
+    public function testFilterOnly() {
+        $this->post('/input/filter/only', [
+            "name" => [
+                "first" => "Fredik",
+                "middle" => "Stefan",
+                "last" => "Jaru"
+            ]
+        ])->assertSeeText("Fredik")->assertSeeText("Jaru")->assertDontSeeText("Stefan");
+    }
+
+    public function testFilterExcept() {
+        $this->post(
+            '/input/filter/except',
+            [
+                "username" => "fredik",
+                "password" => "12345678",
+                "admin" => "true"
+            ]
+        )->assertSeeText("fredik")->assertSeeText("12345678")->assertDontSeeText("true");
+    }
+
+
+    public function testFilterMerge() {
+        $this->post(
+            '/input/filter/merge',
+            [
+                "username" => "fredik",
+                "password" => "12345678",
+                "admin" => "true"
+            ]
+        )->assertSeeText("fredik")->assertSeeText("12345678")->assertSeeText("admin")->assertSeeText("false");;
     }
 }
